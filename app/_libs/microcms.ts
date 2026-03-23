16ee31a80e71a0acc251017bf4905e1721586682
YukiCreateStudio <yuki.createstudio@gmail.com>
2026-03-23 16:57:27 +0900

    P146まで完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,3 +1,21 @@
+import { createClient } from "microcms-js-sdk";
+import {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from "microcms-js-sdk";
+
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
+export type Category = {
+  name: string;
+};
+
 export type News = {
   id: string;
   title: string;
@@ -8,6 +26,23 @@ export type News = {
   createdAt: string;
 };
 
-export type Category = {
-  name: string;
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MIROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MIROCMS_API_KEY,
+});
+
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData;
 };
