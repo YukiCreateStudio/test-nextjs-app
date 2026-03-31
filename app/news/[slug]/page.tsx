e8f5c42aca5489afcf7c54e3f0458caa740c59f6
YukiCreateStudio <yuki.createstudio@gmail.com>
2026-04-01 00:03:38 +0900

    プレビュー設定完了P203

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -5,15 +5,20 @@ import ButtonLink from "@/app/_components/ButtonLink";
 import { notFound } from "next/navigation";
 
 type Props = {
-  params: Promise<{
-    slug: string;
-  }>;
+  params: { slug: string };
+  searchParams: { draftKey?: string };
 };
 
-export default async function Page({ params }: Props) {
-  const { slug } = await params;
-  const data = await getNewsDetail(slug).catch(notFound);
+export default async function Page(props: Props) {
+  const { slug } = await props.params;
+  const { draftKey } = await props.searchParams;
 
+  const data = await getNewsDetail(
+    slug,
+    draftKey ? { draftKey } : undefined,
+  ).catch(notFound);
+  // console.log("slug:", slug);
+  // console.log("draftKey:", draftKey);
   return (
     <>
       <Article data={data} />
